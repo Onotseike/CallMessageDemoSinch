cdb5fcc1763ff59900dce4ccd18bf161c9fb12e9
Onotseike <onotseike@hotmail.com>
2017-06-14 05:55:27 +0100

    Add project files.

---
+++ CallMessageDemo/CallMessageDemo/Interface/ISinchService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallMessageDemo.Interface
+{
+    public interface ISinchService
+    {
+        void StartSinchClient(string username);
+        void StopSinchClient();
+        bool IsSinchClientStarted();
+
+        void SendMessage(string receipient, string message);
+        void CallVoice(string receipientId);
+        void CallVideo(string receipientId);
+
+        void MessageDetails();
+    }
+}
---
+++ CallMessageDemo/CallMessageDemo/InterfaceImplements/SinchService.cs
@@ -0,0 +1,54 @@
+﻿using CallMessageDemo.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CallMessageDemo.InterfaceImplements
+{
+    public class SinchService : ISinchService
+    {
+
+        ISinchService sinchService;// = DependencyService.Get<ISinchService>();
+
+        public SinchService()
+        {
+            sinchService = DependencyService.Get<ISinchService>();
+        }
+
+        public void CallVideo(string receipientId)
+        {
+            sinchService.CallVideo(receipientId);
+        }
+
+        public void CallVoice(string receipientId)
+        {
+            sinchService.CallVoice(receipientId);
+        }
+
+        public bool IsSinchClientStarted()
+        {
+            return sinchService.IsSinchClientStarted();
+        }
+
+        public void MessageDetails()
+        {
+            sinchService.MessageDetails();
+        }
+
+        public void SendMessage(string receipient, string message)
+        {
+            sinchService.SendMessage(receipient, message);
+        }
+
+        public void StartSinchClient(string username)
+        {
+            sinchService.StartSinchClient(username);
+        }
+
+        public void StopSinchClient()
+        {
+            sinchService.StopSinchClient();
+        }
+    }
+}
